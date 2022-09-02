3b78d7498572cd1a333a0c9016793a39342372fe
Gananand Kini <gkini@umd.edu>
2022-09-02 00:10:51 -0400

    Removed old code for SQL injection.

--- WebGoatCore/Data/BlogResponseRepository.cs
+++ WebGoatCore/Data/BlogResponseRepository.cs
@@ -15,10 +15,11 @@ namespace WebGoatCore.Data
         public void CreateBlogResponse(BlogResponse response)
         {
             //TODO: should put this in a try/catch
+            // Use EntityFramework FromSQLRaw for faster query
             var responseBack = _context.BlogResponses.FromSqlRaw(
                 $"INSERT INTO BlogResponses (Author, BlogEntryId, ResponseDate, Contents) VALUES ( '{response.Author}', '{response.BlogEntryId}', '{response.ResponseDate}', '{response.Contents}' ); SELECT * FROM BlogResponses WHERE changes() = 1 AND Id = last_insert_rowid();").ToListAsync();
-            _context.BlogResponses.Add(response);
-            _context.SaveChanges();
+            // _context.BlogResponses.Add(response);
+            // _context.SaveChanges();
         }
     }
 }
